ef97b7342e89709eb3fb0fbbbaac42552a991ffd
Lawrence <lgnjenga@gmail.com>
2022-12-01 22:36:59 +0300

    First commit for class-based React Todo-List App from Traversy Media tutorial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Todos from "./components/Todos";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // State
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Clean room",
+        completed: false,
+      },
+      {
+        id: 2,
+        title: "Wash dishes",
+        completed: false,
+      },
+      {
+        id: 3,
+        title: "Cook food",
+        completed: false,
+      },
+    ],
+  };
+
+  render() {
+    // console.log(this.state.todos);
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
